a5b2de04884f956eef664a881ba1830253d758a2
Abhimanyu <farmacyst@Abhimanyus-MacBook-Air.local>
2025-03-22 19:10:55 +0530

    Refactor fetch_gas_prices function to clean data and remove unused save_to_csv function

--- gas_api.py
+++ gas_api.py
@@ -1,4 +1,4 @@
-#! user/bin/python
+
 import requests
 import pandas as pd
 import json
@@ -45,37 +45,39 @@ def fetch_gas_prices(api_key: str, series_id: str, observation_start: str, obser
     response = requests.get(url)
     if response.status_code == 200:
         gas_data = response.json()["observations"]
-        return pd.DataFrame(gas_data)
+        gas_df = pd.DataFrame(gas_data)
+        gas_df = gas_df.drop(columns=['realtime_start', 'realtime_end'])
+        return gas_df
     else:
         raise Exception(f"Failed to fetch data from FRED API: {response.status_code}")
     
 
 # Function to save DataFrame to CSV
-def save_to_csv(df: pd.DataFrame, filename: str) -> None:
-    """
-    Saves a DataFrame to a CSV file.
+# def save_to_csv(df: pd.DataFrame, filename: str) -> None:
+#     """
+#     Saves a DataFrame to a CSV file.
     
-    Args:
-        df (pd.DataFrame): The DataFrame to save.
-        filename (str): The name of the CSV file.
-    """
-    new_df = pd.DataFrame()
-    new_df['date'] = df['date']
-    new_df['value'] = df['value']
+#     Args:
+#         df (pd.DataFrame): The DataFrame to save.
+#         filename (str): The name of the CSV file.
+#     """
+#     new_df = pd.DataFrame()
+#     new_df['date'] = df['date']
+#     new_df['value'] = df['value']
 
-    new_df.to_csv(filename, index=False)
-    print(f"Data saved to {filename}")
+#     new_df.to_csv(filename, index=False)
+#     print(f"Data saved to {filename}")
 
 # Running the functions
 if __name__ == "__main__":
     # FRED API details
     api_key = "bd7ee64d6749a1bd68c358548a4a8c81"
     series_id = "GASREGW"
-    observation_start = "2024-01-07"
+    observation_start = "2023-12-31"
     observation_end = "2024-12-24"
     # Fetch gas prices
     gas_df = fetch_gas_prices(api_key, series_id, observation_start, observation_end)
     # Save to CSV
-    save_to_csv(gas_df, "data/gas_data.csv")
+    # save_to_csv(gas_df, "data/gas_data.csv")
     # Print the first few rows of the DataFrame
     print(gas_df.head())
